e4b527428f2a968a0007a7daad01db71b94af95f
fearschism <77707954+fearschism@users.noreply.github.com>
2021-05-16 00:59:14 +0300

    Delete BnkGUI directory

--- BnkGUI/Saves--WIP/ReadME.txt
+++
@@ -1,3 +0,0 @@
-
-This right here is where im gonna store all the Clients Details so the system
-can read and write
--- BnkGUI/src/DataHolder.java
+++
@@ -1,7 +0,0 @@
-import Client.Bank;
-import Client.Client;
-
-public class DataHolder {
-	public  Bank bnk = new Bank();
-	public  Client cl;
-}
